4293e564303965c08595a7525eabc3675d6f6fe5
RostiMelk <hello@rosti.no>
2020-09-11 21:20:49 +0200

    cron-job

---
+++ cron-job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Setup a cron job to this file
+# Enter your variables, replace xxx
+# Here is how you setup a cron job on a rpi https://bc-robotics.com/tutorials/setting-cron-job-raspberry-pi/
+
+curl -X DELETE -G \
+'http://localhost/index.php' \
+-d portfolio=xxx \
+-d ifttt=xxx
