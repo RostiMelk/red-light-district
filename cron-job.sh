845290d783c052408967465042603ab8ac071bff
RostiMelk <hello@rosti.no>
2020-09-11 21:49:02 +0200

    Updated comments

--- cron-job.sh
+++ cron-job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-# Setup a cron job to this file
-# Enter your variables, replace xxx
-# Here is how you setup a cron job on a rpi https://bc-robotics.com/tutorials/setting-cron-job-raspberry-pi/
+# Enter your variables, replace xxx in this file
+# Setup a cron job to this file, for example every 10 minutes: 
+# */10 * * * * sh /home/pi/cron-job.sh
+# Here is a full guide on how you setup a cron job on a rpi https://bc-robotics.com/tutorials/setting-cron-job-raspberry-pi/
 
 curl -X GET -G \
 'http://localhost/index.php' \
